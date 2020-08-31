d1f471c5ce4d165fec708c5296bd8d17c5ed7416
Carlos Capitão <carloscapitaoo@hotmail.com>
2020-08-31 12:26:41 +0100

    curso
    configuração da pagina de curso.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@
      easing: 'slide'
  });
 
+ $('#myTab a').on('click', function (e) {
+    e.preventDefault()
+    $(this).tab('show')
+  })
+
  var stack_bar_top = {"dir1": "down", "dir2": "right", "push": "top", "spacing1": 0, "spacing2": 0};
  $('#inscrever').click(function() {
     var notice = new PNotify({
@@ -40,30 +45,7 @@
          scrollProperty: 'scroll'
      });
 
-     $(document).ready(function(){
-        // Add smooth scrolling to all links
-        $("a").on('click', function(event) {
-      
-          // Make sure this.hash has a value before overriding default behavior
-          if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-      
-            // Store hash
-            var hash = this.hash;
-      
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-              scrollTop: $(hash).offset().top
-            }, 800, function(){
-         
-              // Add hash (#) to URL when done scrolling (default click behavior)
-              window.location.hash = hash;
-            });
-          } // End if
-        });
-      });
+     
 
 
      var fullHeight = function() {
