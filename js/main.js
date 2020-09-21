7437ea4747cd9d5a859291761862b53312b89dc3
Carlos Capitão <carloscapitaoo@hotmail.com>
2020-09-21 13:22:02 +0100

    summernote e pagina de perfil do formador.

--- js/main.js
+++ js/main.js
@@ -3,9 +3,22 @@
      easing: 'slide'
  });
 
-
-
-
+$('.summernote').summernote({
+    height: 300, // set editor height
+    minHeight: null, // set minimum height of editor
+    maxHeight: null, // set maximum height of editor
+    focus: false, // set focus to editable area after initializing summernote
+    toolbar: [
+        ['style', ['style']],
+        ['font', ['bold', 'underline', 'clear']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['table', ['table']],
+        ['insert', ['link', 'picture', 'video']],
+        ['view', ['codeview', 'help']]
+      ]
+});
+ 
  $('#myTab a').on('click', function (e) {
     e.preventDefault()
     $(this).tab('show')
