8d4927f6fc713e86e8ed5eca4b4780f8f7c0aa98
Carlos Capitão <carloscapitaoo@hotmail.com>
2020-09-21 14:05:49 +0100

    link

--- js/main.js
+++ js/main.js
@@ -3,21 +3,6 @@
      easing: 'slide'
  });
 
-$('.summernote').summernote({
-    height: 300, // set editor height
-    minHeight: null, // set minimum height of editor
-    maxHeight: null, // set maximum height of editor
-    focus: false, // set focus to editable area after initializing summernote
-    toolbar: [
-        ['style', ['style']],
-        ['font', ['bold', 'underline', 'clear']],
-        ['color', ['color']],
-        ['para', ['ul', 'ol', 'paragraph']],
-        ['table', ['table']],
-        ['insert', ['link', 'picture', 'video']],
-        ['view', ['codeview', 'help']]
-      ]
-});
  
  $('#myTab a').on('click', function (e) {
     e.preventDefault()
---
+++ js/summernote-costom.js
@@ -0,0 +1,15 @@
+$('.summernote').summernote({
+    height: 300, // set editor height
+    minHeight: null, // set minimum height of editor
+    maxHeight: null, // set maximum height of editor
+    focus: false, // set focus to editable area after initializing summernote
+    toolbar: [
+        ['style', ['style']],
+        ['font', ['bold', 'underline', 'clear']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['table', ['table']],
+        ['insert', ['link', 'picture', 'video']],
+        ['view', ['codeview', 'help']]
+      ]
+});
